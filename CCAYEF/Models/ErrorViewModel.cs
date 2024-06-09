70c925262eab6714dca814c65114f72b68ca94ec
Peter Claver <pitamcclav@gmail.com>
2024-06-09 20:42:19 +0300

    Add project files.

---
+++ CCAYEF/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CCAYEF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

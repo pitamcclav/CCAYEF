c74fd43f5fdc2282ee682ec2c97d1fc3d7c22492
Peter Claver <pitamcclav@gmail.com>
2024-06-11 07:40:42 +0300

    Ammended ui

--- CCAYEF/wwwroot/js/site.js
+++ CCAYEF/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+    function updateNavLink() {
+        var navLinks = document.querySelectorAll('.nav-link');
+        navLinks.forEach((link) => {
+            link.classList.remove('active');
+            if (link.href === window.location.href) {
+                link.classList.add('active');
+            }
+        }
+        )
+    }
+    updateNavLink();
+});
+    
